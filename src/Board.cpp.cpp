b292f126f8cad5ae9f569b8f2e3380b456c15d07
AdelAdham <adeladham95@gmail.com>
2017-10-28 15:00:24 +0200

    adding our classes

---
+++ src/Board.cpp.cpp
@@ -0,0 +1,30 @@
+class Board {
+
+
+public:
+
+    unsigned long long white_pawns;
+    unsigned long long white_knights;
+    unsigned long long white_bishops;
+    unsigned long long white_rooks;
+    unsigned long long white_queens;
+    unsigned long long white_king;
+
+    unsigned long long black_pawns;
+    unsigned long long black_knights;
+    unsigned long long black_bishops;
+    unsigned long long black_rooks;
+    unsigned long long black_queens;
+    unsigned long long black_king;
+
+    unsigned long long white_pieces;
+    unsigned long long black_pieces;
+    unsigned long long all_pieces;
+
+    unsigned long long key; // zobrist key
+
+
+
+
+
+};
