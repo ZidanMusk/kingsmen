aa60811ee365179a12997a530bfde10551c45699
AdelAdham <adeladham95@gmail.com>
2017-10-28 15:08:50 +0200

    temp camelCase

--- src/Board.cpp
+++ src/Board.cpp
@@ -3,23 +3,23 @@ class Board {
 
 public:
 
-    unsigned long long white_pawns;
-    unsigned long long white_knights;
-    unsigned long long white_bishops;
-    unsigned long long white_rooks;
-    unsigned long long white_queens;
-    unsigned long long white_king;
+    unsigned long long whitePawns;
+    unsigned long long whiteKnights;
+    unsigned long long whiteBishops;
+    unsigned long long whiteRooks;
+    unsigned long long whiteQueens;
+    unsigned long long whiteKing;
 
-    unsigned long long black_pawns;
-    unsigned long long black_knights;
-    unsigned long long black_bishops;
-    unsigned long long black_rooks;
-    unsigned long long black_queens;
-    unsigned long long black_king;
+    unsigned long long blackPawns;
+    unsigned long long blackKnights;
+    unsigned long long blackBishops;
+    unsigned long long blackRooks;
+    unsigned long long blackQueens;
+    unsigned long long blackKing;
 
-    unsigned long long white_pieces;
-    unsigned long long black_pieces;
-    unsigned long long all_pieces;
+    unsigned long long whitePieces;
+    unsigned long long blackPieces;
+    unsigned long long allPieces;
 
     unsigned long long key; // zobrist key
 
