fded63ad3346a2b3881bf7120e2745400d003a11
omar osama <omarosamafahmy@gmail.com>
2017-10-28 22:27:59 +0200

    try

---
+++ src/d5rtfyuhi.cpp
@@ -0,0 +1,4 @@
+//
+// Created by maverick on 28/10/17.
+//
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "A7A" << std::endl;
     return 0;
 }
