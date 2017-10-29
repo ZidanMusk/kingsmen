dff8cdd9f02aff21b1d7855f669d59c04516cf9c
AdelAdham <adeladham95@gmail.com>
2017-10-29 04:47:31 +0200

    unordered maps

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,7 +1,13 @@
+#include <bits/stdc++.h>
+typedef unsigned long long ull;
+using namespace std;
+
 class Board {
 
 
 public:
+
+    unordered_map <ull,int> zobristTable;
 
     const int CASTLEKINGSIDE = 1;
     const int CASTLEQUEENSIDE = 2;
@@ -13,7 +19,7 @@ public:
 
 
     // Zobrist tables
-    unsigned long long pawn[2][64] = {{0x79ad695501e7d1e8L, 0x8249a47aee0e41f7L, 0x637a7780decfc0d9L, 0x19fc8a768cf4b6d4L, 0x7bcbc38da25a7f3cL, 0x5093417aa8a7ed5eL, 0x7fb9f855a997142L,  0x5355f900c2a82dc7L,
+    ull pawn[2][64] = {{0x79ad695501e7d1e8L, 0x8249a47aee0e41f7L, 0x637a7780decfc0d9L, 0x19fc8a768cf4b6d4L, 0x7bcbc38da25a7f3cL, 0x5093417aa8a7ed5eL, 0x7fb9f855a997142L,  0x5355f900c2a82dc7L,
                                               0xe99d662af4243939L, 0xa49cd132bfbf7cc4L, 0xce26c0b95c980d9L, 0xbb6e2924f03912eaL, 0x24c3c94df9c8d3f6L, 0xdabf2ac8201752fcL, 0xf145b6beccdea195L, 0x14acbaf4777d5776L,
                                               0xf9b89d3e99a075c2L, 0x70ac4cd9f04f21f5L, 0x9a85ac909a24eaa1L, 0xee954d3c7b411f47L, 0x72b12c32127fed2bL, 0x54b3f4fa5f40d873L, 0x8535f040b9744ff1L, 0x27e6ad7891165c3fL,
                                               0x8de8dca9f03cc54eL, 0xff07f64ef8ed14d0L, 0x92237ac237f3859L,  0x87bf02c6b49e2ae9L, 0x1920c04d47267bbdL, 0xae4a9346cc3f7cf2L, 0xa366e5b8c54f48b8L, 0x87b3e2b2b5c907b1L,
@@ -30,7 +36,7 @@ public:
                                               0xfcf7fe8a3430b241L, 0x5c82c505db9ab0faL, 0x51ebdc4ab9ba3035L, 0x9f74d14f7454a824L, 0xbf983fe0fe5d8244L, 0xd310a7c2ce9b6555L, 0x1fcbacd259bf02e7L, 0x18727070f1bd400bL,
                                               0x96d693460cc37e5dL, 0x4de0b0f40f32a7b8L, 0x6568fca92c76a243L, 0x11d505d4c351bd7fL, 0x7ef48f2b83024e20L, 0xb9bc6c87167c33e7L, 0x8c74c368081b3075L, 0x3253a729b9ba3ddeL,
                                               0xec16ca8aea98ad76L, 0x63dc359d8d231b78L, 0x93c5b5f47356388bL, 0x39f890f579f92f88L, 0x5f0f4a5898171bb6L, 0x42880b0236e4d951L, 0x6d2bdcdae2919661L, 0x42e240cb63689f2fL}};;
-    unsigned long long rook[2][64] = {{0xd18d8549d140caeaL, 0x1cfc8bed0d681639L, 0xca1e3785a9e724e5L, 0xb67c1fa481680af8L, 0xdfea21ea9e7557e3L, 0xd6b6d0ecc617c699L, 0xfa7e393983325753L, 0xa09e8c8c35ab96deL,
+    ull rook[2][64] = {{0xd18d8549d140caeaL, 0x1cfc8bed0d681639L, 0xca1e3785a9e724e5L, 0xb67c1fa481680af8L, 0xdfea21ea9e7557e3L, 0xd6b6d0ecc617c699L, 0xfa7e393983325753L, 0xa09e8c8c35ab96deL,
                                               0x7983eed3740847d5L, 0x298af231c85bafabL, 0x2680b122baa28d97L, 0x734de8181f6ec39aL, 0x53898e4c3910da55L, 0x1761f93a44d5aefeL, 0xe4dbf0634473f5d2L, 0x4ed0fe7e9dc91335L,
                                               0x261e4e4c0a333a9dL, 0x219b97e26ffc81bdL, 0x66b4835d9eafea22L, 0x4cc317fb9cddd023L, 0x50b704cab602c329L, 0xedb454e7badc0805L, 0x9e17e49642a3e4c1L, 0x66c1a2a1a60cd889L,
                                               0x36f60e2ba4fa6800L, 0x38b6525c21a42b0eL, 0xf4f5d05c10cab243L, 0xcf3f4688801eb9aaL, 0x1ddc0325259b27deL, 0xb9571fa04dc089c8L, 0xd7504dfa8816edbbL, 0x1fe2cca76517db90L,
@@ -48,7 +54,7 @@ public:
                                               0x3a938fee32d29981L, 0x2c5e9deb57ef4743L, 0x1e99b96e70a9be8bL, 0x764dbeae7fa4f3a6L, 0xaac40a2703d9bea0L, 0x1a8c1e992b941148L, 0x73aa8a564fb7ac9eL, 0x604d51b25fbf70e2L,
                                               0x8fe88b57305e2ab6L, 0x89039d79d6fc5c5cL, 0x9bfb227ebdf4c5ceL, 0x7f7cc39420a3a545L, 0x3f6c6af859d80055L, 0xc8763c5b08d1908cL, 0x469356c504ec9f9dL, 0x26e6db8ffdf5adfeL}};
 
-    unsigned long long knight[2][64] = {{0x3bba57b68871b59dL, 0xdf1d9f9d784ba010L, 0x94061b871e04df75L, 0x9315e5eb3a129aceL, 0x8bd35cc38336615L,  0xfe9a44e9362f05faL, 0x78e37644e7cad29eL, 0xc547f57e42a7444eL,
+    ull knight[2][64] = {{0x3bba57b68871b59dL, 0xdf1d9f9d784ba010L, 0x94061b871e04df75L, 0x9315e5eb3a129aceL, 0x8bd35cc38336615L,  0xfe9a44e9362f05faL, 0x78e37644e7cad29eL, 0xc547f57e42a7444eL,
                                                 0x4f2a5cb07f6a35b3L, 0xa2f61bb6e437fdb5L, 0xa74049dac312ac71L, 0x336f52f8ff4728e7L, 0xd95be88cd210ffa7L, 0xd7f4f2448c0ceb81L, 0xf7a255d83bc373f8L, 0xd2b7adeeded1f73fL,
                                                 0x4c0563b89f495ac3L, 0x18fcf680573fa594L, 0xfcaf55c1bf8a4424L, 0x39b0bf7dde437ba2L, 0xf3a678cad9a2e38cL, 0x7ba2484c8a0fd54eL, 0x16b9f7e06c453a21L, 0x87d380bda5bf7859L,
                                                 0x35cab62109dd038aL, 0x32095b6d4ab5f9b1L, 0x3810e399b6f65ba2L, 0x9d1d60e5076f5b6fL, 0x7a1ee967d27579e2L, 0x68ca39053261169fL, 0x8cffa9412eb642c1L, 0x40e087931a00930dL,
@@ -66,7 +72,7 @@ public:
                                                 0x9ae182c8bc9474e8L, 0xb05ca3f564268d99L, 0xcfc447f1e53c8e1bL, 0x4850e73e03eb6064L, 0x2c604a7a177326b3L, 0xbf692b38d079f23L,  0xde336a2a4bc1c44bL, 0xd7288e012aeb8d31L,
                                                 0x6703df9d2924e97eL, 0x8ec97d2917456ed0L, 0x9c684cb6c4d24417L, 0xfc6a82d64b8655fbL, 0xf9b5b7c4acc67c96L, 0x69b97db1a4c03dfeL, 0xe755178d58fc4e76L, 0xa4fc4bd4fc5558caL}};
 
-    unsigned long long bishop[2][64] = {{0x2fe4b17170e59750L, 0xe8d9ecbe2cf3d73fL, 0xb57d2e985e1419c7L, 0x572b974f03ce0bbL, 0xa8d7e4dab780a08dL, 0x4715ed43e8a45c0aL, 0xc330de426430f69dL, 0x23b70edb1955c4bfL,
+    ull bishop[2][64] = {{0x2fe4b17170e59750L, 0xe8d9ecbe2cf3d73fL, 0xb57d2e985e1419c7L, 0x572b974f03ce0bbL, 0xa8d7e4dab780a08dL, 0x4715ed43e8a45c0aL, 0xc330de426430f69dL, 0x23b70edb1955c4bfL,
                                                 0x49353fea39ba63b1L, 0xf85b2b4fbcde44b7L, 0xbe7444e39328a0acL, 0x3e2b8bcbf016d66dL, 0x964e915cd5e2b207L, 0x1725cabfcb045b00L, 0x7fbf21ec8a1f45ecL, 0x11317ba87905e790L,
                                                 0xe94c39a54a98307fL, 0xaa70b5b4f89695a2L, 0x3bdbb92c43b17f26L, 0xcccb7005c6b9c28dL, 0x18a6a990c8b35ebdL, 0xfc7c95d827357afaL, 0x1fca8a92fd719f85L, 0x1dd01aafcd53486aL,
                                                 0xdbc0d2b6ab90a559L, 0x94628d38d0c20584L, 0x64972d68dee33360L, 0xb9c11d5b1e43a07eL, 0x2de0966daf2f8b1cL, 0x2e18bc1ad9704a68L, 0xd4dba84729af48adL, 0xb7a0b174cff6f36eL,
@@ -84,7 +90,7 @@ public:
                                                 0xbbe83f4ecc2bdecbL, 0xcd454f8f19c5126aL, 0xc62c58f97dd949bfL, 0x693501d628297551L, 0xb9ab4ce57f2d34f3L, 0x9255abb50d532280L, 0xebfafa33d7254b59L, 0xe9f6082b05542e4eL,
                                                 0x98954d51fff6580L,  0x8107fccf064fcf56L, 0x852f54934da55cc9L, 0x9c7e552bc76492fL,  0xe9f6760e32cd8021L, 0xa3bc941d0a5061cbL, 0xba89142e007503b8L, 0xdc842b7e2819e230L}};
 
-    unsigned long long queen[2][64] = {{0x720bf5f26f4d2eaaL, 0x1c2559e30f0946beL, 0xe328e230e3e2b3fbL, 0x87e79e5a57d1d13L,  0x8dd9bdfd96b9f63L,  0x64d0e29eea8838b3L, 0xddf957bc36d8b9caL, 0x6ffe73e81b637fb3L,
+    ull queen[2][64] = {{0x720bf5f26f4d2eaaL, 0x1c2559e30f0946beL, 0xe328e230e3e2b3fbL, 0x87e79e5a57d1d13L,  0x8dd9bdfd96b9f63L,  0x64d0e29eea8838b3L, 0xddf957bc36d8b9caL, 0x6ffe73e81b637fb3L,
                                                0x93b633abfa3469f8L, 0xe846963877671a17L, 0x59ac2c7873f910a3L, 0x660d3257380841eeL, 0xd813f2fab7f5c5caL, 0x4112cf68649a260eL, 0x443f64ec5a371195L, 0xb0774d261cc609dbL,
                                                0xb5635c95ff7296e2L, 0xed2df21216235097L, 0x4a29c6465a314cd1L, 0xd83cc2687a19255fL, 0x506c11b9d90e8b1dL, 0x57277707199b8175L, 0xcaf21ecd4377b28cL, 0xc0c0f5a60ef4cdcfL,
                                                0x7c45d833aff07862L, 0xa5b1cfdba0ab4067L, 0x6ad047c430a12104L, 0x6c47bec883a7de39L, 0x944f6de09134dfb6L, 0x9aeba33ac6ecc6b0L, 0x52e762596bf68235L, 0x22af003ab672e811L,
@@ -101,7 +107,7 @@ public:
                                                0x758f450c88572e0bL, 0x1b6baca2ae4e125bL, 0x61cf4f94c97df93dL, 0x2738259634305c14L, 0xd39bb9c3a48db6cfL, 0x8215e577001332c8L, 0xa1082c0466df6c0aL, 0xef02cdd06ffdb432L,
                                                0x7976033a39f7d952L, 0x106f72fe81e2c590L, 0x8c90fd9b083f4558L, 0xfd080d236da814baL, 0x7b64978555326f9fL, 0x60e8ed72c0dff5d1L, 0xb063e962e045f54dL, 0x959f587d507a8359L,
                                                0x1a4e4822eb4d7a59L, 0x5d94337fbfaf7f5bL, 0xd30c088ba61ea5efL, 0x9d765e419fb69f6dL, 0x9e21f4f903b33fd9L, 0xb4d8f77bc3e56167L, 0x733ea705fae4fa77L, 0xa4ec0132764ca04bL}};
-    unsigned long long king[2][64] = {{0x2102ae466ebb1148L, 0xe87fbb46217a360eL, 0x310cb380db6f7503L, 0xb5fdfc5d3132c498L, 0xdaf8e9829fe96b5fL, 0xcac09afbddd2cdb4L, 0xb862225b055b6960L, 0x55b6344cf97aafaeL,
+    ull king[2][64] = {{0x2102ae466ebb1148L, 0xe87fbb46217a360eL, 0x310cb380db6f7503L, 0xb5fdfc5d3132c498L, 0xdaf8e9829fe96b5fL, 0xcac09afbddd2cdb4L, 0xb862225b055b6960L, 0x55b6344cf97aafaeL,
                                               0x46e3ecaaf453ce9L,  0x962aceefa82e1c84L, 0xf5b4b0b0d2deeeb4L, 0x1af3dbe25d8f45daL, 0xf9f4892ed96bd438L, 0xc4c118bfe78feaaeL, 0x7a69afdcc42261aL,  0xf8549e1a3aa5e00dL,
                                               0x486289ddcc3d6780L, 0x222bbfae61725606L, 0x2bc60a63a6f3b3f2L, 0x177e00f9fc32f791L, 0x522e23f3925e319eL, 0x9c2ed44081ce5fbdL, 0x964781ce734b3c84L, 0xf05d129681949a4cL,
                                               0xd586bd01c5c217f6L, 0x233003b5a6cfe6adL, 0x24c0e332b70019b0L, 0x9da058c67844f20cL, 0xe4d9429322cd065aL, 0x1fab64ea29a2ddf7L, 0x8af38731c02ba980L, 0x7dc7785b8efdfc80L,
@@ -119,54 +125,54 @@ public:
                                               0xabbdcdd7ed5c0860L, 0x9853eab63b5e0b35L, 0x352787baa0d7c22fL, 0xc7f6aa2de59aea61L, 0x3727073c2e134b1L,  0x5a0f544dd2b1fb18L, 0x74f85198b05a2e7dL, 0x963ef2c96b33be31L,
                                               0xff577222c14f0a3aL, 0x4e4b705b92903ba4L, 0x730499af921549ffL, 0x13ae978d09fe5557L, 0xd9e92aa246bf719eL, 0x7a4c10ec2158c4a6L, 0x49cad48cebf4a71eL, 0xcf05daf5ac8d77b0L}};
 
-    unsigned long long whiteKingSideCastling = 0x31d71dce64b2c310L;
-    unsigned long long whiteQueenSideCastling = 0xf165b587df898190L;
-    unsigned long long blackKingSideCastling = 0xa57e6339dd2cf3a0L;
-    unsigned long long blackQueenSideCastling = 0x1ef6e6dbb1961ec9L;
-    unsigned long long passantColumn[8] = {0x70cc73d90bc26e24L, 0xe21a6b35df0c3ad7L, 0x3a93d8b2806962L,
+    ull whiteKingSideCastling = 0x31d71dce64b2c310L;
+    ull whiteQueenSideCastling = 0xf165b587df898190L;
+    ull blackKingSideCastling = 0xa57e6339dd2cf3a0L;
+    ull blackQueenSideCastling = 0x1ef6e6dbb1961ec9L;
+    ull passantColumn[8] = {0x70cc73d90bc26e24L, 0xe21a6b35df0c3ad7L, 0x3a93d8b2806962L,
                                            0x1c99ded33cb890a1L, 0xcf3145de0add4289L, 0xd0e4427a5514fb72L,
                                            0x77c621cc9fb3a483L, 0x67a34dac4356550bL};
-    unsigned long long whiteMove = 0xf8d626aaaf278509L;
+    ull whiteMove = 0xf8d626aaaf278509L;
 
     // board representation
 
-    unsigned long long whitePawns;
-    unsigned long long whiteKnights;
-    unsigned long long whiteBishops;
-    unsigned long long whiteRooks;
-    unsigned long long whiteQueens;
-    unsigned long long whiteKing;
+    ull whitePawns;
+    ull whiteKnights;
+    ull whiteBishops;
+    ull whiteRooks;
+    ull whiteQueens;
+    ull whiteKing;
 
-    unsigned long long blackPawns;
-    unsigned long long blackKnights;
-    unsigned long long blackBishops;
-    unsigned long long blackRooks;
-    unsigned long long blackQueens;
-    unsigned long long blackKing;
+    ull blackPawns;
+    ull blackKnights;
+    ull blackBishops;
+    ull blackRooks;
+    ull blackQueens;
+    ull blackKing;
 
-    unsigned long long whitePieces;
-    unsigned long long blackPieces;
-    unsigned long long allPieces;
+    ull whitePieces;
+    ull blackPieces;
+    ull allPieces;
 
-    unsigned long long key; // zobrist key
+    ull key; // zobrist key
 
-    unsigned long long white_pawn_history[];
-    unsigned long long white_knight_history[];
-    unsigned long long white_bishop_history[];
-    unsigned long long white_rook_history[];
-    unsigned long long white_queen_history[];
-    unsigned long long white_king_history[];
+    ull white_pawn_history[];
+    ull white_knight_history[];
+    ull white_bishop_history[];
+    ull white_rook_history[];
+    ull white_queen_history[];
+    ull white_king_history[];
 
-    unsigned long long black_pawn_history[];
-    unsigned long long black_knight_history[];
-    unsigned long long black_bishop_history[];
-    unsigned long long black_rook_history[];
-    unsigned long long black_queen_history[];
-    unsigned long long black_king_history[];
+    ull black_pawn_history[];
+    ull black_knight_history[];
+    ull black_bishop_history[];
+    ull black_rook_history[];
+    ull black_queen_history[];
+    ull black_king_history[];
 
-    unsigned long long white_pieces_history[];
-    unsigned long long black_pieces_history[];
-    unsigned long long all_pieces_history[];
+    ull white_pieces_history[];
+    ull black_pieces_history[];
+    ull all_pieces_history[];
 
     //bool[] whiteToMove_history;
     //int[] fiftyMoveRule_history;
@@ -179,15 +185,15 @@ public:
     bool blackCastleQ_history[];
     bool white_has_castled_history[];
     bool black_has_castled_history[];
-    unsigned long long key_history[];
+    ull key_history[];
 
     //should be considered at implementation
-    unsigned long long whiteCastleK;
-    unsigned long long whiteCastleQ;
-    unsigned long long blackCastleK;
-    unsigned long long blackCastleQ;
-    unsigned long long enPassantLoc;
-    unsigned long long whiteToMove;
+    ull whiteCastleK;
+    ull whiteCastleQ;
+    ull blackCastleK;
+    ull blackCastleQ;
+    ull enPassantLoc;
+    ull whiteToMove;
 
     int moveNumber = 0;
 
@@ -211,7 +217,7 @@ public:
 
 
     // Zobrist hash generators
-    unsigned long long squareZKey(int location, char piece) {
+    ull squareZKey(int location, char piece) {
         switch (piece) {
             case 'p':
                 return pawn[0][location];
@@ -241,12 +247,12 @@ public:
         return 0;
     }
 
-    unsigned long long ZMove(int from, int to, char piece) {
+    ull ZMove(int from, int to, char piece) {
         return squareZKey(from, piece) ^ squareZKey(to, piece);
     }
 
-    unsigned long long ZBoard() {
-        unsigned long long key = 0;
+    ull ZBoard() {
+        ull key = 0;
 
         for (int loc = 0; loc < 64; loc++) {
             key ^= squareZKey(loc, getPieceAt(loc));
@@ -270,11 +276,11 @@ public:
         return key;
     }
 
-    bool locExist(unsigned long long mask, unsigned long long check) {
+    bool locExist(ull mask, ull check) {
         return (mask & check) != 0;
     }
 
-    void unsetBit(unsigned long long &mask, unsigned long long bit) {
+    void unsetBit(ull &mask, ull bit) {
         mask &= (~(bit));
     }
 
@@ -416,7 +422,13 @@ public:
 
     bool isDraw() {}
 
-    bool isMate() {}
+    bool isMate() {
+
+        if(!validMovesKing)
+            return true;
+        else
+            return false;
+    }
 
     bool isEndOfGame() { return (isMate() || isDraw()); }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <bits/stdc++.h>
 #include "Board.cpp"
 
 int main() {
