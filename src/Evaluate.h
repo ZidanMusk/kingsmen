0cc967b01c5c3b88f6a47eeaa62468f3cb18c2d6
SlippinKareem <kareem.m.mohsen@gmail.com>
2017-11-08 17:36:01 +0200

    King safety added to evaluation class and deleted from the board class

--- src/Evaluate.h
+++ src/Evaluate.h
@@ -11,6 +11,7 @@ public:
     Evaluate(Board *b);
 
     int pawnStructureEval(int color);   //0 for white, 1 for black
+    int kingSafty(int blackKingSafety, int whiteKingSafty); // arguments are calculated in mobility
 };
 
 #endif //KINGSMEN_EVALUATE_H
