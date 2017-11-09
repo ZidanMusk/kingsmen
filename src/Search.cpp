76e3deb53139cc9c5df6e531b90c304e45002e95
Mohamed-Hossam <eng.mohamed.hosaam@gmail.com>
2017-11-09 23:02:25 +0200

    Qsearch added

--- src/Search.cpp
+++ src/Search.cpp
@@ -3,3 +3,31 @@
 Search::Search(int maxDepth)
 {this->_MaxDepth=maxDepth;}
 pair<int,int> Search::GetBestMove(int state_id) {return this->_IterativeDeepening(state_id,this->_MaxDepth);}
+int Search::Qsearch(int state_id,int alpha, int beta,bool isMax) {
+    int score=this->Eval.GetScore(state_id);
+    if(isMax)
+    {
+        if(score>=beta)
+            return score;
+        alpha=max(alpha,score);
+        vector<int>capturingMoves=this->Eval.GetCapturingMoves(state_id);
+        for (int i = 0; i < (capturingMoves.size())&&score<beta; ++i) {
+            score=max(score,Qsearch(capturingMoves[i],alpha,beta,!isMax));
+            alpha=max(score,alpha);
+        }
+        return score;
+    }
+    else
+    {
+        if(score<=  alpha)
+            return score;
+        beta=min(beta,score);
+        vector<int>capturingMoves=this->Eval.GetCapturingMoves(state_id);
+        for (int i = 0; i < (capturingMoves.size())&&score>alpha; ++i) {
+            score=min(score,Qsearch(capturingMoves[i],alpha,beta,!isMax));
+            beta=min(score,beta);
+        }
+        return score;
+    }
+
+}
--- src/Search.h
+++ src/Search.h
@@ -2,21 +2,25 @@
 #define CHESSSEARCH_SEARCH_H
 #define oo  1e9
 #include "External/Extern.h"
+#include "External/ExternBig.h"
 #include "Node.cpp"
 #include <unordered_map>
+#include <map>
 #include <iostream>
 #include <algorithm>
 #include <set>
+#include <time.h>
 using namespace std;
 
 class Search {
 public:
+    ExternBig Eval;
     Search(int maxDepth);
     pair<int,int> GetBestMove(int state_id);
 protected:
-    Extern Eval;
     int _MaxDepth;
     virtual pair<int,int> _IterativeDeepening(int rootID,int MaxDepth)=0;
+    int Qsearch(int state_id,int alpha,int beta,bool isMax);
 };
 
 
