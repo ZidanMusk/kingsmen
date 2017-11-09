22880237060b32a3b6977903b1d6f34058298fa8
Mohamed-Hossam <eng.mohamed.hosaam@gmail.com>
2017-11-09 22:54:25 +0200

    updated iterative deepening table

--- src/MTDF.cpp
+++ src/MTDF.cpp
@@ -6,10 +6,31 @@
 
 bool MTDF::_MySortFunMax(pair<int,int> i, pair<int,int> j) {return i.second>j.second;}
 bool MTDF::_MySortFunMin(pair<int,int> i, pair<int,int> j) {return i.second<j.second;}
-vector<int> MTDF::_GetSortedPossibleMoves(int state_id) {
-    if(_DeepeningTableLastIter.find(state_id)!=_DeepeningTableLastIter.end())
-        return _DeepeningTableLastIter[state_id];
+vector<int> MTDF::_GetSortedPossibleMoves(int state_id,int IsMax) {
     vector<int> Pmoves=Search::Eval.GetPossibleMoves(state_id);
+    return Pmoves;
+    vector<pair<int,int>>Pmoves_State_Score;
+    for (int i = 0; i < Pmoves.size(); ++i) {
+        int score;
+        if(_DeepeningTable.find({state_id,Pmoves[i]})!=_DeepeningTable.end())
+            score=_DeepeningTable[{state_id,Pmoves[i]}];
+        else
+        {
+            if(IsMax)
+                score=-oo;
+            else
+                score= oo;
+
+        }
+        Pmoves_State_Score.push_back({Pmoves[i],score});
+
+    }
+    if(IsMax)sort(Pmoves_State_Score.begin(),Pmoves_State_Score.end(),_MySortFunMax);
+    else sort(Pmoves_State_Score.begin(),Pmoves_State_Score.end(),_MySortFunMin);
+    Pmoves.clear();
+    for (int j = 0; j < Pmoves_State_Score.size(); ++j) {
+        Pmoves.push_back(Pmoves_State_Score[j].first);
+    }
     return Pmoves;
 }
 void MTDF::_UpdateNode(int state_id, int g, int alpha, int beta) {
@@ -23,15 +44,16 @@ void MTDF::_UpdateNode(int state_id, int g, int alpha, int beta) {
 }
 pair<int,int> MTDF::_IterativeDeepening(int root_id,int MaxDepth)
 {
+    clock_t tStart = clock();
     int firstguess = 0;
     for (int d = 1;d<=MaxDepth; d++) {
         firstguess = this->_MTDF(root_id, firstguess, d);
     }
-    pair<int,int> Bibi=make_pair(firstguess,_DeepeningTableLastIter[1][0]);
-    _DeepeningTableLastIter.clear();
-    _DeepeningTableCurIter.clear();
+    pair<int,int> Bibi={firstguess,-55};
     _MTDfTable.clear();
-    cout<<"EvalStates : "<<this->EvalStates<<" Of 33"<<endl;
+    _DeepeningTable.clear();
+    cout<<"EvalStates : ["<<EvalStates<<","<<OpenedStates<<"] in "<<clock() - tStart-Eval.TreeCreationTime<<" ms"<<endl;
+
     return Bibi;
 }
 int MTDF::_MTDF(int root_id, int f, int d)
@@ -40,24 +62,23 @@ int MTDF::_MTDF(int root_id, int f, int d)
     int lowerbound = -oo;
     int upperbound = +oo;
     this->_MTDfTable.clear();
-    this->_DeepeningTableCurIter.clear();
     while (lowerbound < upperbound) {
         int beta = (g == lowerbound) ? g + 1 : g;
-        g = this->_AlphaBetaWithMemory(root_id, beta - 1, beta, d,true,d);
+        g = this->_AlphaBetaWithMemory(root_id, beta - 1, beta, d,true,d, true);
         if (g < beta) {
             upperbound = g;
         } else {
             lowerbound = g;
         }
     }
-    this->_DeepeningTableLastIter=this->_DeepeningTableCurIter;
     return g;
 }
-int MTDF::_AlphaBetaWithMemory(int state_id, int alpha, int beta,int d,bool IsMax,int MaxDepth) {
+int MTDF::_AlphaBetaWithMemory(int state_id, int alpha, int beta,int d,bool IsMax,int MaxDepth,bool DoNull) {
 
 
     Node entry;
-    int g,a,b;
+    OpenedStates++;
+    int g,_g,a,b;
     if (_MTDfTable.find(state_id)!=_MTDfTable.end()) {
         entry=_MTDfTable[state_id];
         if (entry.lowerbound >= beta) {
@@ -69,7 +90,6 @@ int MTDF::_AlphaBetaWithMemory(int state_id, int alpha, int beta,int d,bool IsMa
         alpha = max(alpha, entry.lowerbound);
         beta  = min(beta, entry.upperbound);
     } else {
-        OpenedStates++;
         _MTDfTable[state_id]=entry;
     }
 
@@ -80,21 +100,28 @@ int MTDF::_AlphaBetaWithMemory(int state_id, int alpha, int beta,int d,bool IsMa
         else
         {
             EvalStates++;
-            g = Search::Eval.GetScore(state_id);
+            g = Search::Qsearch(state_id,alpha,beta,IsMax);
         }
         this->_UpdateNode(state_id,g,alpha,beta);
         return g;
     }
 
-    vector<int>Pmoves=this->_GetSortedPossibleMoves(state_id);
-    vector<pair<int,int>>Pmoves_State_Score;
+    vector<int>Pmoves=this->_GetSortedPossibleMoves(state_id,IsMax);
     if(IsMax)
     {
+        /*if(d>=3&&DoNull)
+        {
+            int NullSCore=_AlphaBetaWithMemory(state_id, alpha, beta, d-3,!IsMax,MaxDepth,false);
+            if(NullSCore>=beta)
+                return NullSCore;
+        }*/
+
         g = -oo;
         a = alpha;
         for (int i = 0; (i < Pmoves.size())&&(g<beta); ++i) {
-            g = max(g,_AlphaBetaWithMemory(Pmoves[i], a, beta, d-1,!IsMax,MaxDepth));
-            Pmoves_State_Score.push_back(make_pair(Pmoves[i],g));
+            _g=_AlphaBetaWithMemory(Pmoves[i], a, beta, d-1,!IsMax,MaxDepth,true);
+            //_DeepeningTable[{state_id,Pmoves[i]}]=_g;
+            g = max(g,_g);
             a = max(a,g);
         }
     } else
@@ -102,18 +129,13 @@ int MTDF::_AlphaBetaWithMemory(int state_id, int alpha, int beta,int d,bool IsMa
         g = +oo;
         b = beta;
         for (int i = 0; (i < Pmoves.size())&&(g>alpha); ++i) {
-            g = min(g,_AlphaBetaWithMemory(Pmoves[i], alpha, b, d-1,!IsMax,MaxDepth));
-            Pmoves_State_Score.push_back(make_pair(Pmoves[i],g));
+            _g=_AlphaBetaWithMemory(Pmoves[i], alpha, b, d-1,!IsMax,MaxDepth,true);
+            //_DeepeningTable[{state_id,Pmoves[i]}]=_g;
+            g = min(g,_g);
             b = min(b,g);
         }
     }
-    if(IsMax)sort(Pmoves_State_Score.begin(),Pmoves_State_Score.end(),_MySortFunMax);
-    else sort(Pmoves_State_Score.begin(),Pmoves_State_Score.end(),_MySortFunMin);
-    vector<int>tmp;
-    for (int j = 0; j < Pmoves_State_Score.size(); ++j) {
-        tmp.push_back(Pmoves_State_Score[j].first);
-    }
-    _DeepeningTableCurIter[state_id]=tmp;
+
 
     this->_UpdateNode(state_id,g,alpha,beta);
     return g;
--- src/MTDF.h
+++ src/MTDF.h
@@ -10,13 +10,13 @@ public:
     MTDF(int maxDepth):Search(maxDepth) {};
 private:
     unordered_map<int,Node>_MTDfTable;
-    unordered_map<int,vector<int>>_DeepeningTableLastIter;
-    unordered_map<int,vector<int>>_DeepeningTableCurIter;
+    unordered_map<int,Node>_VisitedState;
+    map<pair<int,int>,int>_DeepeningTable;
     pair<int,int> _IterativeDeepening(int root_id,int MaxDepth);
     int _MTDF(int root_id, int f, int d);
-    int _AlphaBetaWithMemory(int state_id, int alpha, int beta,int d,bool IsMax,int MaxDepth);
+    int _AlphaBetaWithMemory(int state_id, int alpha, int beta,int d,bool IsMax,int MaxDepth,bool DoNull);
     void _UpdateNode(int state_id,int g ,int alpha, int beta);
-    vector<int> _GetSortedPossibleMoves(int state_id);
+    vector<int> _GetSortedPossibleMoves(int state_id,int IsMax);
     static bool _MySortFunMax(pair<int,int> i, pair<int,int> j);
     static bool _MySortFunMin(pair<int,int> i, pair<int,int> j);
 };
