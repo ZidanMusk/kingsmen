183893aec2d062ba6a939e58534557875177c7c1
moemen95 <runmoemen@gmail.com>
2017-11-21 22:41:56 +0200

    Evaluate moemen 04

--- src/Evaluate.h
+++ src/Evaluate.h
@@ -3,6 +3,8 @@
 
 #include "Board.cpp"
 
+
+
 class Evaluate {
 
     Board *_board;
@@ -41,17 +43,26 @@ class Evaluate {
 public:
     explicit Evaluate(Board *b);
 
-    int pawnStructureEval(int color);   //0 for white, 1 for black
-    int kingSafety(int blackKingSafety, int whiteKingSafty); // arguments are calculated in mobility
-
-
     /// @brief Evaluates a given position.
     /// @return The heuristic score given to the position.
     int evaluate();
 
+    int pawnStructureEval(int color);   //0 for white, 1 for black
+    int kingSafety(int blackKingSafety, int whiteKingSafty); // arguments are calculated in mobility
+
+    int interpolateScore(int scoreOp, int scoreEd, int phase)
+
     int getPstScore();
 
     int mobilityEval(std::array<int, 2>& kingSafetyScore, int phase);
+
+    inline short getPieceSquareTableOp(Piece p, Square sq) {
+        return mPieceSquareTableOpening[p][sq];
+    }
+
+    inline short getPieceSquareTableEd(Piece p, Square sq) {
+        return mPieceSquareTableEnding[p][sq];
+    }
 
 };
 
