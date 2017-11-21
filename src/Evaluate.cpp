e58a5305eff6ab55febd703b3e6d0b57d9d106f6
AdelAdham <adeladham95@gmail.com>
2017-11-21 23:31:00 +0200

    solving errors

--- src/Evaluate.cpp
+++ src/Evaluate.cpp
@@ -104,6 +104,28 @@ Evaluate::Evaluate(Board *b) : doubledPenaltyOpening({
             mPieceSquareTableEnding[p + Color::Black * 6][sq ^ 56] = -(endingPST[p][sq] + pieceValuesEnding[p]);
         }
     }
+
+    files = {
+            0x0101010101010101,
+            0x0202020202020202,
+            0x0404040404040404,
+            0x0808080808080808,
+            0x1010101010101010,
+            0x2020202020202020,
+            0x4040404040404040,
+            0x8080808080808080
+    };
+
+    ranks = {
+            0x00000000000000FF,
+            0x000000000000FF00,
+            0x0000000000FF0000,
+            0x00000000FF000000,
+            0x000000FF00000000,
+            0x0000FF0000000000,
+            0x00FF000000000000,
+            0xFF00000000000000
+    };
 }
 
 int Evaluate::pawnStructureEval(int color) {
--- src/Evaluate.h
+++ src/Evaluate.h
@@ -13,27 +13,9 @@ class Evaluate {
 
     const int doubledPenaltyEnding[8];
 
-    ull files[] = {
-        0x0101010101010101,
-                0x0202020202020202,
-                0x0404040404040404,
-                0x0808080808080808,
-                0x1010101010101010,
-                0x2020202020202020,
-                0x4040404040404040,
-                0x8080808080808080
-    };
+    ull files[];
 
-    ull ranks[] = {
-        0x00000000000000FF,
-                0x000000000000FF00,
-                0x0000000000FF0000,
-                0x00000000FF000000,
-                0x000000FF00000000,
-                0x0000FF0000000000,
-                0x00FF000000000000,
-                0xFF00000000000000
-    };
+    ull ranks[];
 
     // These two have to be annoyingly static, as we use them in position.cpp to incrementally update the PST eval.
     static std::array<std::array<short, 64>, 12> mPieceSquareTableOpening;
@@ -50,7 +32,7 @@ public:
     int pawnStructureEval(int color);   //0 for white, 1 for black
     int kingSafety(int blackKingSafety, int whiteKingSafty); // arguments are calculated in mobility
 
-    int interpolateScore(int scoreOp, int scoreEd, int phase)
+    int interpolateScore(int scoreOp, int scoreEd, int phase);
 
     int getPstScore();
 
